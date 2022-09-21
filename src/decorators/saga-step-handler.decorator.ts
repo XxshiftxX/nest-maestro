1026e08f92bcd321e66d7b3effbe96d97de85f6d
BuildTools <unconfigured@null.spigotmc.org>
2022-09-21 14:46:42 +0900

    변경된 이름 반영

--- src/decorators/saga-step-handler.decorator.ts
+++ src/decorators/saga-step-handler.decorator.ts
@@ -1,9 +1,9 @@
 import 'reflect-metadata';
 import { v4 } from 'uuid';
-import { SagaStep } from '../interfaces/saga-step.interface';
+import { ISagaStep } from '../interfaces/saga-step.interface';
 import { SAGA_STEP_HANDLER_METADATA, SAGA_STEP_METADATA } from './constants';
 
-export const SagaStepHandler = (step: SagaStep): ClassDecorator => (target: object) => {
+export const SagaStepHandler = (step: ISagaStep): ClassDecorator => (target: object) => {
   if (!Reflect.hasMetadata(SAGA_STEP_METADATA, step)) {
     Reflect.defineMetadata(SAGA_STEP_METADATA, { id: v4() }, step);
   }
--- src/interfaces/saga-step-handler.interface.ts
+++ src/interfaces/saga-step-handler.interface.ts
@@ -1,6 +1,6 @@
-import { SagaStep } from './saga-step.interface';
+import { ISagaStep } from './saga-step.interface';
 
-export interface ISagaStepHandler<TStep extends SagaStep = any, TResult = any> {
+export interface ISagaStepHandler<TStep extends ISagaStep = any, TResult = any> {
   execute(step: TStep): Promise<TResult>;
   reject?(step: TStep): Promise<void>;
 }
